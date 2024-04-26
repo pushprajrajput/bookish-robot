ace81fca467fc8dfcfdc7d4e474dc42a4eec41cf
Pushpraj Rajput <pushpdfs@gmail.com>
2024-04-26 14:10:48 +0530

    Implemented singy linked list.

--- src/data_structures/singly_linked_list.ts
+++ src/data_structures/singly_linked_list.ts
@@ -18,8 +18,14 @@ export class SinglyLinkedList<T> {
   }
 
   append(val: T) {
+    //Steps
+    //1.  Create new node
+    //2.  Check if head is null, if yes make new node as head and tail as null head means linked list is empty
+    //3.  If head does exist then check if tail is also there,
+    //3.1 If tail exist means we need to attach new node to next element of tail and make new node as tail
+    //3.2 If tail does not exist then make new node as tail
     const newNode = new ListNode(val);
-    if (this.head === null) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -31,8 +37,12 @@ export class SinglyLinkedList<T> {
   }
 
   prepend(val: T) {
+    //Steps
+    //1. Create new node
+    //2. Check if head is null, null head means linked list is empty and new node needs to be made head and tail
+    //3. Else make new head a next element of next node and make new node a new head
     const newNode = new ListNode(val);
-    if (this.head === null) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -41,11 +51,48 @@ export class SinglyLinkedList<T> {
     }
   }
 
+  addAtIndex(index: number, val: T) {
+    //Steps
+    //1. If index is zero means we need to prepend
+    //2. Create new node and start travesring till node before given index to find current node after where add needs to be performed
+    //3. Check if current is null, if null return as index given is out of the list
+    //4. Make new node's next as current node's next
+    //5. Make new node's next as current  node's next and make current node's next as new node
+    if (index === 0) {
+      this.prepend(val);
+      return;
+    }
+
+    const newNode = new ListNode(val);
+    let current = this.head;
+    let currentIndex = 0;
+
+    while (current != null && currentIndex < index - 1) {
+      current = current.next;
+      currentIndex++;
+    }
+
+    if (current === null) {
+      return;
+    }
+
+    newNode.next = current.next;
+    current.next = newNode;
+
+    if (newNode.next === null) {
+      this.tail = newNode;
+    }
+  }
+
   delete(val: T) {
-    //If list is empty return
+    // Steps
+    //1. If head is empty return as linked list is null
+    //2. Check from head if value matches forward head unitl heads value does not match
+    //3. If value is in middle use while loop checking on next eliment defined if match found make next's next as current's next
+    //4. Update current eliment as tail if tail value matches with value to be deleted
+
     if (!this.head) return;
 
-    //Check from head if value matches forward head unitl heads value does nit match
     while (this.head && this.head.val === val) {
       this.head = this.head.next;
     }
